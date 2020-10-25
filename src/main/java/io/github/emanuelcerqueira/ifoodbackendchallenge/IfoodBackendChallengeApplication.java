a5938ce1942c2656ce1efdc5c4a3d1f62394c89e
Emanuel Cerqueira <emanuelcerqueira07@gmail.com>
2020-10-25 11:14:00 -0300

    Project setup

---
+++ src/main/java/io/github/emanuelcerqueira/ifoodbackendchallenge/IfoodBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package io.github.emanuelcerqueira.ifoodbackendchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodBackendChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfoodBackendChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/emanuelcerqueira/ifoodbackendchallenge/IfoodBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.emanuelcerqueira.ifoodbackendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoodBackendChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
